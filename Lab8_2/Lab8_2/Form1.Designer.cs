5b06ccba5da2d0f7cb4158cb21e1127365c09d4a
VuNgocDuyKhanh <102215602+VuNgocDuyKhanh@users.noreply.github.com>
2023-04-20 14:52:14 +0700

    Add files via upload

---
+++ Lab8_2/Lab8_2/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Lab8_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Question = new System.Windows.Forms.TextBox();
+            this.ShakeBtn = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Question
+            // 
+            this.Question.Location = new System.Drawing.Point(12, 12);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(776, 22);
+            this.Question.TabIndex = 0;
+            // 
+            // ShakeBtn
+            // 
+            this.ShakeBtn.Location = new System.Drawing.Point(12, 40);
+            this.ShakeBtn.Name = "ShakeBtn";
+            this.ShakeBtn.Size = new System.Drawing.Size(157, 23);
+            this.ShakeBtn.TabIndex = 1;
+            this.ShakeBtn.Text = "Shake The Magic 8 Ball";
+            this.ShakeBtn.UseVisualStyleBackColor = true;
+            this.ShakeBtn.Click += new System.EventHandler(this.ShakeBtn_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.Location = new System.Drawing.Point(12, 69);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(776, 22);
+            this.Answer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.ShakeBtn);
+            this.Controls.Add(this.Question);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Question;
+        private System.Windows.Forms.Button ShakeBtn;
+        private System.Windows.Forms.TextBox Answer;
+    }
+}
+
---
+++ Lab8_2/Lab8_2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8_2
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        List<String> answers = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+            //Affirmative answers
+            answers.Add("It is certain.");
+            answers.Add("It is decidedly so.");
+            answers.Add("Without a doubt.");
+            answers.Add("Yes definitely.");
+            answers.Add("You may rely on it.");
+            answers.Add("As I see it, yes.");
+            answers.Add("Most likely.");
+            answers.Add("Outlook good.");
+            answers.Add("Yes.");
+            answers.Add("Signs point to yes.");
+            //Non-committal answers
+            answers.Add("Reply hazy, try again.");
+            answers.Add("Ask again later.");
+            answers.Add("Better not tell you now.");
+            answers.Add("Cannot predict now.");
+            answers.Add("Concentrate and ask again.");
+            //Negative answers
+            answers.Add("Don't count on it.");
+            answers.Add("My reply is no.");
+            answers.Add("My sources say no.");
+            answers.Add("Outlook not so good.");
+            answers.Add("Very doubtful.");
+        }
+
+        private void ShakeBtn_Click(object sender, EventArgs e)
+        {
+            int index = rnd.Next(answers.Count);
+            Answer.Text = answers[index];
+        }
+    }
+}
